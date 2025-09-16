1bbb7269bf6c990391ec032adddd6067daf0a910
allanndunda <allanndunda>
2025-09-16 14:18:14 +0300

    added

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+// code your solution h
+
+function superbowlWin(record){
+    const win = record.find(game => game.result === "W")
+    return win ? win.year : undefined;
+}
